c5ba21455d2288271fa874f945434768b62c33fd
L4L <arch4life@users.noreply.github.com>
2018-12-13 15:22:25 -0300

    Update setup-autologin.sh

--- docs/autologin/setup-autologin.sh
+++ docs/autologin/setup-autologin.sh
@@ -2,10 +2,8 @@
 
 # Autologin SystemD
 
-sudo mkdir -p /usr/lib/systemd/system/
-sudo cp ./autologin@.service /usr/lib/systemd/system/
-export nnu=`whoami`
-sudo sed sed -i "s/zzzz/$nnu/" /usr/lib/systemd/system/autologin@.service
+export nnu=`whoami` ; sed -i "s/zzzz/$nnu/" ./autologin@.service
+sudo mkdir -p /usr/lib/systemd/system/ ; sudo cp ./autologin@.service /usr/lib/systemd/system/
 sudo systemctl enable autologin@tty1 ; sudo systemctl disable -f {getty@tty1,lxdm,lightdm,slim,sddm,gdm}
 exit
 
