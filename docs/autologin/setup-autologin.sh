731ff233cac6a0a6b4d46fcc32d63e7ddf27510d
bino-linux <19884860+binolinux@users.noreply.github.com>
2019-06-16 19:23:03 -0300

    Update setup-autologin.sh

--- docs/autologin/setup-autologin.sh
+++ docs/autologin/setup-autologin.sh
@@ -2,9 +2,10 @@
 
 # Autologin SystemD
 
+wget 'https://github.com/binolinux/misc/raw/master/docs/autologin/autologin%40.service' -O autologin@.service
 export nnu=`whoami` ; sed -i "s/zzzz/$nnu/" ./autologin@.service
 sudo chmod 755 ./autologin@.service ; sudo chown root:root ./autologin@.service
 sudo mkdir -p /usr/lib/systemd/system/ ; sudo mv ./autologin@.service /usr/lib/systemd/system/
 sudo systemctl enable autologin@tty1 ; sudo systemctl disable -f {getty@tty1,lxdm,lightdm,slim,sddm,gdm}
-exit
 
+exit
