6413a136d2011e702322c45aebb065bc66f64688
bino-linux <19884860+binolinux@users.noreply.github.com>
2022-01-07 21:29:07 -0300

    Update instala-firefox.sh

--- docs/instala-firefox.sh
+++ docs/instala-firefox.sh
@@ -20,7 +20,7 @@ sudo pacman -Syu --needed --noconfirm extra/firefox extra/dbus-glib
 sudo rm -f /usr/lib/firefox/browser/features/{doh-rollout@mozilla.org.xpi,screenshots@mozilla.org.xpi,webcompat-reporter@mozilla.org.xpi,webcompat@mozilla.org.xpi}
 #sudo cp -ar usr/lib/firefox/browser/extensions /usr/lib/firefox/browser
 sudo ln -sf /usr/lib/firefox/firefox /usr/local/bin/browser
-sudo rm -f /usr/share/applications/firefox.desktop
+#sudo rm -f /usr/share/applications/firefox.desktop
 cat << EOF > $HOME/.local/share/applications/firefox.desktop
 [Desktop Entry]
 Version=1.0
@@ -57,10 +57,10 @@ if [[ $RDK = [sSyY] ]]; then
 timeout 6s firefox
 DEFAULT=`ls $HOME/.mozilla/firefox | grep default-release | sed -n '1p'`
 cd "$LOCAL"
-mkdir -p "$LOCAL"/perfil/extensions ; rm -f "$LOCAL"/perfil/browsec@browsec.com.xpi
-wget --content-disposition https://addons.mozilla.org/firefox/downloads/file/3886236 -O "$LOCAL"/perfil/extensions/uBlock0@raymondhill.net.xpi
+mkdir -p "$LOCAL"/perfil/extensions ; rm -f "$LOCAL"/perfil/browsec@browsec.com.xpi ; rm -f "$LOCAL"/perfil/langpack-pt-BR@firefox.mozilla.org.xpi ; rm -f "$LOCAL"/perfil/uBlock0@raymondhill.net.xpi
 wget --content-disposition https://addons.mozilla.org/firefox/downloads/file/3869267 -O "$LOCAL"/perfil/extensions/browsec@browsec.com.xpi
-wget --content-disposition https://addons.mozilla.org/firefox/downloads/file/3883426  -O "$LOCAL"/perfil/extensions/langpack-pt-BR@firefox.mozilla.org.xpi
+wget --content-disposition https://addons.mozilla.org/firefox/downloads/file/3883426 -O "$LOCAL"/perfil/extensions/langpack-pt-BR@firefox.mozilla.org.xpi
+wget --content-disposition https://addons.mozilla.org/firefox/downloads/file/3886236 -O "$LOCAL"/perfil/extensions/uBlock0@raymondhill.net.xpi
 echo 'user_pref("browser.download.dir", "'$HOME'/.cache");' >> "$LOCAL"/perfil/prefs.js
 rm -rf $HOME/.mozilla/firefox/"$DEFAULT" ; mkdir -p $HOME/.local/bin $HOME/.mozilla/firefox/"$DEFAULT"
 cp -rT "$LOCAL"/perfil/ $HOME/.mozilla/firefox/"$DEFAULT"
